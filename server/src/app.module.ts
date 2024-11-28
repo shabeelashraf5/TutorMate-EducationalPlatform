0a2aca15fedcac6ab499733940f4182da51842e7
shabeelashraf5 <05shebz@gmail.com>
2024-11-28 19:52:15 +0530

    added env and config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb+srv://shabeelash5:wGzEMe0qZvBVAgN6@cluster0.kfyrp1e.mongodb.net/?retryWrites=true&w=majority',
-    {dbName: 'tutorme'}
-  )],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+  }), MongooseModule.forRootAsync({
+
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      uri: configService.get<string>('DATABASE_URI'),
+      dbName: configService.get<string>('DATABASE_NAME')
+    })
+  }),
+
+  AuthModule, ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
