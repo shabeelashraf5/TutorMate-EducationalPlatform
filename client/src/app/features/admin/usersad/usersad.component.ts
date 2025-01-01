d9225df2e93e7d0099f53be74ecfb7c917e2be14
shabeelashraf5 <05shebz@gmail.com>
2025-01-01 22:06:41 +0530

    Modified Admin Side

--- client/src/app/features/admin/usersad/usersad.component.ts
+++ client/src/app/features/admin/usersad/usersad.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { AdusersService } from '../../../core/service/admin/adusers/adusers.service';
 import { Register } from '../../../models/register.model';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usersad',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './usersad.component.html',
   styleUrl: './usersad.component.css',
 })
@@ -16,12 +17,40 @@ export class UsersadComponent implements OnInit {
   dialogMessage: string = '';
   actionType: string = '';
   userDetails: Register[] = [];
+  isModalOpen: boolean = false
+  formData!: FormGroup
+  selectedFiles: File[] = [];
+  isVisible: boolean = false;
+  showEditDialoge: boolean = false
 
   useradService = inject(AdusersService);
+
+  classes = [
+    { value: '1', label: 'Class I' },
+    { value: '2', label: 'Class II' },
+    { value: '3', label: 'Class III' },
+    { value: '4', label: 'Class IV' },
+    { value: '5', label: 'Class V' },
+    { value: '6', label: 'Class VI' },
+    { value: '7', label: 'Class VII' },
+    { value: '8', label: 'Class VIII' },
+    { value: '9', label: 'Class IX' },
+    { value: '10', label: 'Class X' },
+    { value: '11', label: 'Class XI' },
+    { value: '12', label: 'Class XII' },
+  ];
+
 
   ngOnInit() {
     this.loadUsers();
   }
+
+   myForm() {
+      this.formData = new FormGroup({
+        title: new FormControl('', [Validators.required]),
+        class: new FormControl('', [Validators.required]),
+      });
+    }
 
   loadUsers() {
     this.useradService.displayUserDetails().subscribe({
@@ -60,4 +89,21 @@ export class UsersadComponent implements OnInit {
     console.log(`User has confirmed to ${this.actionType} the user`);
     this.closeDialog();
   }
+
+  toggleModal() {
+    this.showEditDialoge = !this.showEditDialoge;
+  }
+
+  closeDialogEdit() {
+    this.showEditDialoge = false;
+  }
+
+  createFolder() {
+
+  }
+
+  onFileChange(event: Event): void {
+
+  }
+
 }
